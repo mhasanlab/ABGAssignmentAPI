1decbbbf0be9a9645bf638e617f56ea496148ee3
mhasanlab <mainul9996@gmail.com>
2023-10-21 01:04:19 +0600

    App setting json file Change

--- ABGAssignmentAPI/ABGAssignment/Program.cs
+++ ABGAssignmentAPI/ABGAssignment/Program.cs
@@ -57,7 +57,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", builder =>
     {
-        builder.WithOrigins("http://127.0.0.1:5500") // Replace with the actual origin of your HTML page
+        builder.WithOrigins("http://127.0.0.1:5500") 
                .AllowAnyHeader()
                .AllowAnyMethod();
     });
